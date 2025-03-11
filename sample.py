ddf7f7b12c3e307716aef6d93468261fd2b550d7
TARUN GUPTA <tarungupta.iiserkol@gmail.com>
2025-03-11 13:10:52 +0100

    loaded path for the data and loaded to the dataframe

--- sample.py
+++ sample.py
@@ -8,15 +8,18 @@ import kagglehub
 path = kagglehub.dataset_download("mssmartypants/rice-type-classification")
 print("Path to dataset files:", path)
 
-import torch
+import torch #part of torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
-from torchsummary import summary
-from sklearn.model_selection import train_test_split
+from torchsummary import summary #part of torchsummary
+from sklearn.model_selection import train_test_split #part of scikit-learn
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu' #check if gpu is there or cpu is there
 print(device)
+
+data_df = pd.read_csv(path +"\\riceClassification.csv")
+print(data_df)
